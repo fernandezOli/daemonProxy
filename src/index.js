16a50220a1d1a8a805363301189dc02a9089c2d9
fernandezOli <braingen@free.fr>
2024-12-20 11:40:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-import daemonProxy from '../daemonProxy';
+//import daemonProxy from '../daemonProxy';
 
-daemonProxy.getJsons();
+await getJsons();
+
+// get json file from daemons
+export async function getJsons() {
+	let jsonDaemons = [];
+	for (let i = 0; i < configFile.daemonList.length; i++) {
+		try {
+			const response = await fetch(configFile.daemonList[i]);
+			if (!response.ok) continue;
+			const loadedConfig = await response.json();
+			jsonDaemons.push(loadedConfig);
+		} catch(error) {
+			if (error.cause.code === 'ECONNREFUSED') continue;
+			console.error('-- ERROR json (code):', error.cause.code);
+			console.error('-- ERROR json (cause):', error.cause);
+			//console.error('-- ERROR json:', error);
+			continue;
+		}
+	}
+	return JSON.stringify(jsonDaemons);
+}
